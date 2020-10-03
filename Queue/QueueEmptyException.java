2134ef3eb0b01a3eeee0d1657c98bee446cb9ba0
Harsh <32234204+harsh2000yadav@users.noreply.github.com>
2020-10-03 21:06:08 +0530

    Update QueueEmptyException.java

--- Queue/QueueEmptyException.java
+++ Queue/QueueEmptyException.java
@@ -6,7 +6,14 @@ public class QueueEmptyException extends Exception {
 	 * 
 	 */
 	private static final long serialVersionUID = 891791791902052128L;
-
+	
+	static void queueMethod() {
+    		int x = 5;
+		for(int i=0; i<5; i++){
+			x++;
+		}
+  	} 
+	//adding a new method 
 	
 
 }
